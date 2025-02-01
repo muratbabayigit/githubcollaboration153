8caf988f7178f0895da6596bbd9a04b205f75cbc
babayigit <babayigit@gmail.com>
2025-02-01 19:07:20 +0300

    mv2

--- src/Ortak_Class.java
+++ src/Ortak_Class.java
@@ -1,5 +1,6 @@
 public class Ortak_Class {
     public static void main(String[] args) {
         System.out.println("Ortak Class");
+        System.out.println("Buralar karışacak vaziyet alın");
     }
 }

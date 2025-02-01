3061dda541fc78dc93869b42809c80c1fdf3a388
babayigit <babayigit@gmail.com>
2025-02-01 19:55:26 +0300

    v4

--- src/Ortak_Class.java
+++ src/Ortak_Class.java
@@ -6,7 +6,7 @@ public class Ortak_Class {
         System.out.println("selamlar");
         System.out.println("Buralar karışacak vaziyet alın");
         System.out.println("BURADAN BABAYIGIT GECTI");
-        
+        System.out.println("merhaba");
 
     }
 

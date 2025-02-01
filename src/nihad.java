0f62db58a5a083442803faad312ee4fcc19c9b2d
Nihad <sdetnihad@gmail.com>
2025-02-01 17:50:11 +0100

    v8

--- src/nihad.java
+++ src/nihad.java
@@ -2,5 +2,9 @@ public class nihad {
     public static void main(String[] args) {
         System.out.println("nihad11");
         System.out.println(" hello");
+
+
+
+        System.out.println(" deneme nihad");
     }
 }

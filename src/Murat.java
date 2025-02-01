75edebf388fb3b5f5ac8372515b1b82626380d30
babayigit <babayigit@gmail.com>
2025-02-01 18:27:12 +0300

    first commit

---
+++ src/Murat.java
@@ -0,0 +1,5 @@
+public class Murat {
+    public static void main(String[] args) {
+        System.out.println("Ne yaparsan yap AŞK ile yap!");
+    }
+}
---
+++ src/Ortak_Class.java
@@ -0,0 +1,5 @@
+public class Ortak_Class {
+    public static void main(String[] args) {
+        System.out.println("Ortak Class");
+    }
+}
